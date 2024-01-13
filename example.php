1be7cd0e12671a3ea53bdcabf3d222a0281ef380
shipdoctor ? indaysara <buttercup@mielon.me>
2024-01-13 16:09:07 +0800

    example file
    same directory with the main script file.

---
+++ example.php
@@ -0,0 +1,9 @@
+<?php
+
+$ccgen = new ccheckApi;
+
+$bin = '542418'; //sample bin
+echo $ccgen->generateCreditCardNumber($bin) .'|'. $ccgen->generateCVV();
+// 5424180000000000|123 (sample output) this is a sample output, you can use this to test your script.
+
+?>
